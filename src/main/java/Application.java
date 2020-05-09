77115a4d58908e25f43655bc83c127a9fa062e70
Prakash <prakasshjashnani25@gmail.com>
2020-05-09 17:13:19 +0530

    Add a Controller For URI /hello And add support for get methods based on query paramaters.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,27 +4,38 @@ import java.net.InetSocketAddress;
 
 import com.prakash.controller.GreetingGetController;
 import com.sun.net.httpserver.HttpServer;
+
 @SuppressWarnings("restriction")
 public class Application {
 
-	private static final int SERVER_PORT=8000;
-	private static final String CONTEXT_ROOT="/api/";
+	private static final int SERVER_PORT = 8000;
+	private static final String CONTEXT_ROOT = "/api/";
+	private static  final String GREETING_CONTROLLER="/api/hello";
 	public static void main(String[] args) throws IOException {
-		HttpServer httpServer=HttpServer.create(new InetSocketAddress(SERVER_PORT),0);
-		
-			
-		httpServer.createContext(CONTEXT_ROOT,(exchange -> {
-			final int INVALID_REQUEST_MTHOD=405;
-			if("GET".equals(exchange.getRequestMethod())){
-					new GreetingGetController().getGreeting(exchange);
-			}else {
-				exchange.sendResponseHeaders(INVALID_REQUEST_MTHOD, -1);
-			}
-			
+		HttpServer httpServer = HttpServer.create(new InetSocketAddress(SERVER_PORT), 0);
+
+		httpServer.createContext(CONTEXT_ROOT, (exchange -> {
+			System.out.println("Deefault Cotext Root");
+			final String responseMessage = createResponseMessage();
+			exchange.sendResponseHeaders(200, responseMessage.getBytes().length);
+			OutputStream os = exchange.getResponseBody();
+			os.write(responseMessage.getBytes());
+			os.flush();
+			os.close();
+			exchange.close();
+
 		}));
-		//Default Executor
+		httpServer.createContext(GREETING_CONTROLLER,new GreetingGetController());
+		
+		// Default Executor
 		httpServer.setExecutor(null);
 		httpServer.start();
+	}
+
+	private static String createResponseMessage() {
+		String response = "Welcome !!! ";
+		response += "Available Options we have in our API are - \\n 1. \\api\\Hello?name=<name>";
+		return response;
 	}
 
 }
--- src/main/java/com/prakash/controller/GreetingGetController.java
+++ src/main/java/com/prakash/controller/GreetingGetController.java
@@ -2,22 +2,76 @@ package com.prakash.controller;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 
 @SuppressWarnings("restriction")
-public class GreetingGetController {
+/*
+ * Controller For all Request /hello
+ */
+public class GreetingGetController implements HttpHandler {
 
-	public void getGreeting(HttpExchange exchange) throws IOException {
-		final String responseTest="Welcome  To First Rest Api With out Any frameowk!!!";
-		final int SUCCESS_RESPONSE_CODE=200;
-		exchange.sendResponseHeaders(SUCCESS_RESPONSE_CODE, responseTest.getBytes().length);
-		OutputStream os  =exchange.getResponseBody();
-		os.write(responseTest.getBytes());
-		os.flush();
-		os.close();
-		exchange.close();	// TODO Auto-generated method stub
-		
+	private static final int REQUEST_METHOD_NOT_SUPPORTED = 405;
+
+	private static final int SUCCESS_RESPONSE_CODE = 200;
+
+	@Override
+	public void handle(HttpExchange exchange) throws IOException {
+		if ("GET".equals(exchange.getRequestMethod())) {
+			String responseTest = getGreeting(getQueryMap(exchange.getRequestURI().getRawQuery()));
+			exchange.sendResponseHeaders(SUCCESS_RESPONSE_CODE, responseTest.getBytes().length);
+			OutputStream os = exchange.getResponseBody();
+			os.write(responseTest.getBytes());
+			os.flush();
+			os.close();
+			exchange.close();
+
+		} else {
+			exchange.sendResponseHeaders(REQUEST_METHOD_NOT_SUPPORTED, -1);
+		}
+
+	}
+
+	public String getGreeting(Map<String, List<String>> queryMap) throws IOException {
+
+		String user = queryMap.get("name") == null ? "Annonymous!" : queryMap.get("name").get(0);
+
+		final String responseTest = "Welcome " + user + " To First Rest Api With out Any frameowk!!!";
+		return responseTest;
+
+	}
+
+	private Map<String, List<String>> getQueryMap(String query) {
+		if (query == null || query.trim().equals(""))
+			return Collections.emptyMap();
+
+		Map<String, List<String>> queryStringMap = Pattern.compile("&").splitAsStream(query)
+				.map(s -> Arrays.copyOf(s.split("="), 2))
+				.collect(Collectors.groupingBy(s -> s[0], Collectors.mapping(s -> s[1], Collectors.toList())));
+
+		/*
+		 * Map<String, List<String[]>> collect =
+		 * Pattern.compile("&").splitAsStream(query) .map(s ->
+		 * Arrays.copyOf(s.split("="), 2)) .collect(Collectors.groupingBy(s->s[0]));
+		 * 
+		 * collect.forEach((k,v)->{ System.out.print("\n\n\n KEY "+k); v.forEach(arr->{
+		 * System.out.print("Values - Length "+arr.length+" first value "+arr[0]); });
+		 * });
+		 * 
+		 * 
+		 * queryStringMap.forEach((x,y)->{ System.out.print("\n\n\n Key "+x +" Value ");
+		 * y.forEach(s->{ System.out.print(" "+s); }); });
+		 */
+
+		return queryStringMap;
 	}
 
 }
